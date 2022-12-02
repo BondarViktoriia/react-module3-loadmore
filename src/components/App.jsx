58bfb461b809dd88d2419e1c11dcc68f81e0fab5
BondarViktoriia <103380251+BondarViktoriia@users.noreply.github.com>
2022-12-02 14:49:59 +0200

    add load more

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,49 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    page: 1,
+    query: '',
+    items: [],
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState({
+      page: 1,
+      query: e.target.elements.query.value,
+      items: [],
+    });
+    e.target.reset();
+  };
+
+  loadMore = () => {
+    this.setState(prevState => ({
+      page: prevState.page +1
+    }))
+  }
+
+  componentDidUpdate(_, prevState) {
+
+    if (
+      prevState.page !== this.state.query ||
+      prevState.page !== this.state.page
+    ) {
+      console.log('Fetch data');
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <input type="text" name="query" />
+            <button type="submit">Search</button>
+          </form>
+          <button onClick={this.loadMore}>Load More</button>
+        </div>
+      </>
+    );
+  }
+}
